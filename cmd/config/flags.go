e98af857dfd04dfa9357c1bcdb07c4c9501d492f
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-10-15 13:46:04 +0300

    added env variables usage

--- cmd/config/flags.go
+++ cmd/config/flags.go
@@ -5,11 +5,16 @@ import (
 	"os"
 )
 
+const (
+	serverAddressKey = "SERVER_ADDRESS"
+	baseURLKey       = "BASE_URL"
+)
+
 func SetupFlags() {
 	configFlags := flag.NewFlagSet("Configuration flag set", flag.ContinueOnError)
 
-	serverPath := configFlags.String(BaseConfig.ServerPath.CliParameterName, BaseConfig.ServerPath.Value, "Server path")
-	baseURL := configFlags.String(BaseConfig.ShoretnedBaseURL.CliParameterName, BaseConfig.ShoretnedBaseURL.Value, "Base URL for shortened links")
+	serverPath := obtainServerPath(configFlags)
+	baseURL := obtainBaseURL(configFlags)
 
 	configFlags.Parse(os.Args[1:])
 
@@ -17,4 +22,24 @@ func SetupFlags() {
 		BaseConfig.ServerPath.Value = *serverPath
 		BaseConfig.ShoretnedBaseURL.Value = *baseURL
 	}
+}
+
+func obtainServerPath(flags *flag.FlagSet) *string {
+	var serverPath *string
+	if path := os.Getenv(serverAddressKey); path != "" {
+		serverPath = &path
+	} else {
+		serverPath = flags.String(BaseConfig.ServerPath.CliParameterName, BaseConfig.ServerPath.Value, "Server path")
+	}
+	return serverPath
+}
+
+func obtainBaseURL(flags *flag.FlagSet) *string {
+	var baseURL *string
+	if path := os.Getenv(baseURLKey); path != "" {
+		baseURL = &path
+	} else {
+		baseURL = flags.String(BaseConfig.ShoretnedBaseURL.CliParameterName, BaseConfig.ShoretnedBaseURL.Value, "Base url for shortened links")
+	}
+	return baseURL
 }
