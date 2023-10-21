ff22d5a98462edf4cb10aa8851cb95617ef78dd7
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-10-21 11:24:00 +0300

    moved handler to internal dir

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,54 +1,20 @@
 package main
 
 import (
-	"io"
 	"log"
 	"net/http"
 
 	"github.com/YuryBogdanov/link-shortener/internal/config"
-	"github.com/YuryBogdanov/link-shortener/internal/storage"
+	"github.com/YuryBogdanov/link-shortener/internal/handler"
 	"github.com/go-chi/chi/v5"
 )
-
-func handleNewLinkRegistration(w http.ResponseWriter, r *http.Request) {
-	if url, err := io.ReadAll(r.Body); err == nil {
-		linkID, err := storage.MakeAndStoreShortURL(string(url))
-		if err != nil {
-			handleError(w)
-			return
-		}
-		resultLink := getShortenedLink(r, linkID)
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(resultLink))
-	} else {
-		handleError(w)
-	}
-}
-
-func handleExistingLinkRequest(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Path[1:]
-	if link, exists := storage.Links[query]; exists {
-		w.Header().Add("Location", link)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-	} else {
-		handleError(w)
-	}
-}
-
-func getShortenedLink(r *http.Request, linkID string) string {
-	return config.BaseConfig.ShoretnedBaseURL.Value + "/" + linkID
-}
-
-func handleError(w http.ResponseWriter) {
-	w.WriteHeader(http.StatusBadRequest)
-}
 
 func main() {
 	config.SetupFlags()
 
 	router := chi.NewRouter()
-	router.Get("/{id}", handleExistingLinkRequest)
-	router.Post("/", handleNewLinkRegistration)
+	router.Get("/{id}", handler.HandleExistingLinkRequest)
+	router.Post("/", handler.HandleNewLinkRegistration)
 
 	baseURL := config.BaseConfig.ServerPath.Value
 	err := http.ListenAndServe(baseURL, router)
