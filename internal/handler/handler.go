244e55adf9812b60a452c1738a9c0e66d4327109
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-10-27 21:26:04 +0300

    fixed logic

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -25,7 +25,7 @@ func HandleNewLinkRegistration(w http.ResponseWriter, r *http.Request) {
 
 func HandleExistingLinkRequest(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Path[1:]
-	if link, err := storage.GetLinkForKey(query); err != nil {
+	if link, err := storage.GetLinkForKey(query); err == nil {
 		w.Header().Add("Location", link)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	} else {
