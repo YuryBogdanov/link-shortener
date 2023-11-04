152818710ebd2145dc15f367ba20474a70241d7b
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-11-04 23:27:55 +0300

    implemented new endpoint

--- internal/handler/shorten.go
+++ internal/handler/shorten.go
@@ -1,7 +1,46 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/YuryBogdanov/link-shortener/internal/model"
+	"github.com/YuryBogdanov/link-shortener/internal/storage"
+)
 
 func HandleShortenRequest(w http.ResponseWriter, r *http.Request) {
-	// TODO
+	var reqModel model.ShortenRequest
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		handleError(w)
+		return
+	}
+	defer r.Body.Close()
+
+	unmarshalErr := json.Unmarshal(body, &reqModel)
+	if unmarshalErr != nil {
+		handleError(w)
+		return
+	}
+
+	link, err := storage.MakeAndStoreShortURL(reqModel.URL)
+	if err != nil {
+		handleError(w)
+		return
+	}
+
+	response := prepareResponse(w, link)
+	w.Header().Add("Content-Type", "application/json")
+	w.Write(response)
+}
+
+func prepareResponse(w http.ResponseWriter, link string) []byte {
+	result := model.Result{Result: link}
+	bytes, err := json.Marshal(result)
+	if err != nil {
+		handleError(w)
+	}
+	return bytes
 }
