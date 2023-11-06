ebd232c9be7205fc60c92b8550fb786c53b6224f
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-11-06 17:37:28 +0300

    added log type

---
+++ internal/logger/logType.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"log"
+	"os"
+)
+
+type LogType string
+
+const (
+	LogTypeProd LogType = "prod"
+	LogTypeTest LogType = "test"
+)
+
+func getCurrentLogType() LogType {
+	typeAsString := os.Getenv("SHORTENER_ENVIRONMENT")
+	switch typeAsString {
+	case string(LogTypeProd):
+		return LogTypeProd
+
+	case string(LogTypeTest):
+		return LogTypeTest
+	}
+	log.Fatal("unrecognized logger type, returning prod logger as default")
+	return LogTypeProd
+}
