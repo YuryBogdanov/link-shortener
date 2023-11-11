91d6eb44e3dd3eefd4e3873dcd16f525a1ae57f4
Yury Bogdanov <yury-bogdanov@mail.ru>
2023-11-11 14:48:13 +0300

    fixed new link registration logic by adding incoming url validation

--- internal/handler/linkRegistration.go
+++ internal/handler/linkRegistration.go
@@ -3,20 +3,27 @@ package handler
 import (
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/YuryBogdanov/link-shortener/internal/storage"
 )
 
 func HandleNewLinkRegistration(w http.ResponseWriter, r *http.Request) {
-	if url, err := io.ReadAll(r.Body); err == nil {
-		link, err := storage.MakeAndStoreShortURL(string(url))
-		if err != nil {
-			handleError(w)
-			return
-		}
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(link))
-	} else {
+	payload, payloadErr := io.ReadAll(r.Body)
+	if payloadErr != nil {
 		handleError(w)
+		return
 	}
+	url, urlErr := url.ParseRequestURI(string(payload))
+	if urlErr != nil {
+		handleError(w)
+		return
+	}
+	link, linkErr := storage.MakeAndStoreShortURL(string(url.String()))
+	if linkErr != nil {
+		handleError(w)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(link))
 }
